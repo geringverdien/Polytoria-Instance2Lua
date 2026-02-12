7e36e26959e3179038c1c04d06f188f599197c8b
Eli <43004098+geringverdien@users.noreply.github.com>
2026-02-12 18:53:33 +0100

    Revert Config to demo game settings

--- Instance2Lua.lua
+++ Instance2Lua.lua
@@ -3,7 +3,7 @@ local __module_env = {}
 -- polybundle: module dev/lib/Config.module.lua
 __module_env["dev/lib/Config.module.lua"] = (function()
 return {
-    Target = game["Hidden"]:FindChild("AdminGUI"), -- the instance to serialize
+    Target = game["Environment"]:FindChild("Test"), -- the instance to serialize
     Minify = false, -- removes line seperation and shortens variable names
     LocalVars = true, -- create local variables for instances (set to false for large models to avoid hitting the local variable limit)
 	WarningTime = 5 -- time in seconds before the output box is focused so you have time to click into the game window, close menus, etc.
